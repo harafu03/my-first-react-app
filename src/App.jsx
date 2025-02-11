22e73ba667ee5f039866d4023c27525bd62b99c9
KimHakSeong1203 <rmsp0030@gmail.com>
2025-02-12 08:09:25 +0900

    sibal

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = ({title}) => {
+  return(
+    <div style={{
+      border: '1px solid #4b5362',
+      padding: '20px',
+      margin: '10px',
+      backgroundColor: '#31363f',
+      borderRadius: '10px',
+      minHeight: '100px'
+    }}>
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className='card-container'>
+      <Card title="Star Wars" rating={5} isCool={true}/>
+      <Card title="Avatar"/>
+      <Card title="The Lion King"/>
+    </div>
+    
+  )
+}
+
+export default App
